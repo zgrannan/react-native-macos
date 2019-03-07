07bec8b8da537c5ffb1f91b3a91e6e7d1d34a42b
REDMOND\acoates <acoates>
2019-03-06 20:35:24 -0800

    fix

--- .ado/publish.js
+++ .ado/publish.js
@@ -1,16 +1,17 @@
-require('shelljs/global');
-const fs = require('fs');
-const path = require('path');
-const execSync = require('child_process').execSync;
+// Used to publish this fork of react-native
+// Publish it as an attached tar asset to the GitHub release for general consumption, since we can't publish this to the npmjs npm feed
+
+const fs = require("fs");
+const path = require("path");
+const execSync = require("child_process").execSync;
 
 function exec(command) {
   try {
     console.log(`Running command: ${command}`);
     return execSync(command, {
-      stdio: 'inherit'
-  });
-  }
-  catch(err) {
+      stdio: "inherit"
+    });
+  } catch (err) {
     process.exitCode = 1;
     console.log(`Failure running: ${command}`);
     throw err;
@@ -18,27 +19,23 @@ function exec(command) {
 }
 
 function doPublish() {
-
   const publishBranchName = process.env.publishBranchName;
 
   const tempPublishBranch = `publish-${Date.now()}`;
 
-  const pkgJsonPath = path.resolve(__dirname, '../package.json');
-  let pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
+  const pkgJsonPath = path.resolve(__dirname, "../package.json");
+  let pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, "utf8"));
 
   let releaseVersion = pkgJson.version;
 
   const versionGroups = /(.*-microsoft\.)([0-9]*)/.exec(releaseVersion);
   if (versionGroups) {
-    releaseVersion = versionGroups[1] + (parseInt(versionGroups[2])+1);
+    releaseVersion = versionGroups[1] + (parseInt(versionGroups[2]) + 1);
   } else {
-    if (releaseVersion.indexOf('-') === -1)
-    {
-      releaseVersion = releaseVersion + '-microsoft.0';
-    }
-    else
-    {
-      console.log('Invalid version to publish');
+    if (releaseVersion.indexOf("-") === -1) {
+      releaseVersion = releaseVersion + "-microsoft.0";
+    } else {
+      console.log("Invalid version to publish");
       exit(1);
     }
   }
@@ -54,27 +51,114 @@ function doPublish() {
   exec(`git push origin HEAD:${tempPublishBranch} --follow-tags --verbose`);
 
   // -------- Generating Android Artifacts with JavaDoc
-  exec('gradlew installArchives');
+  exec("gradlew installArchives");
 
   // undo uncommenting javadoc setting
-  exec('git checkout ReactAndroid/gradle.properties');
+  exec("git checkout ReactAndroid/gradle.properties");
 
   // Configure npm to publish to internal feed
-  const npmrcPath = path.resolve(__dirname, '../.npmrc');
-  const npmrcContents = `registry=https:${process.env.publishnpmfeed}/registry/\nalways-auth=true`;
+  const npmrcPath = path.resolve(__dirname, "../.npmrc");
+  const npmrcContents = `registry=https:${
+    process.env.publishnpmfeed
+  }/registry/\nalways-auth=true`;
   console.log(`Creating ${npmrcPath} for publishing`);
   fs.writeFileSync(npmrcPath, npmrcContents);
 
   exec(`npm publish`);
   exec(`del ${npmrcPath}`);
-  exec(`git tag v${releaseVersion}`);
-  exec(`git push origin HEAD:${tempPublishBranch} --follow-tags --verbose`);
-  exec(`git checkout ${publishBranchName}`);
-  exec('git pull origin ${publishBranchName}');
-  exec('git merge ${publishBranchName} --no-edit');
-  exec('git push origin HEAD:${publishBranchName} --follow-tags --verbose');
-  exec('git branch -d ${tempPublishBranch}');
-  exec('git push origin --delete -d ${tempPublishBranch}');
+
+  // Push tar to GitHub releases
+  exec(`npm pack`);
+
+  const npmTarPath = path.resolve(
+    __dirname,
+    `../react-native-${releaseVersion}.tgz`
+  );
+  const assetUpdateUrl = `https://uploads.github.com/repos/Microsoft/react-native/releases/{id}/assets?name=react-native-${releaseVersion}.tgz`;
+  const authHeader =
+    "Basic " + new Buffer(":" + process.env.githubToken).toString("base64");
+  const userAgent = "Microsoft-React-Native-Release-Agent";
+
+  let uploadReleaseAssetUrl = "";
+  exec("npm install request@^2.69.0 --no-save");
+
+  const request = require("request");
+
+  const uploadTarBallToRelease = function() {
+    request.post(
+      {
+        url: uploadReleaseAssetUrl,
+        headers: {
+          "User-Agent": userAgent,
+          Authorization: authHeader,
+          "Content-Type": "application/octet-stream"
+        },
+        formData: {
+          file: fs.createReadStream(npmTarPath)
+        }
+      },
+      function(err, res, body) {
+        if (err) {
+          console.error(err);
+          process.exitCode = 1;
+          throw err;
+        }
+
+        var formattedResponse = JSON.parse(body);
+
+        if (formattedResponse.errors) {
+          process.exitCode = 1;
+          console.error(formattedResponse.errors);
+          throw formattedResponse.errors;
+        }
+
+        exec(`del ${npmTarPath}`);
+        exec(`git tag v${releaseVersion}`);
+        exec(
+          `git push origin HEAD:${tempPublishBranch} --follow-tags --verbose`
+        );
+        exec(`git checkout ${publishBranchName}`);
+        exec(`git pull origin ${publishBranchName}`);
+        exec(`git merge ${publishBranchName} --no-edit`);
+        exec(
+          `git push origin HEAD:${publishBranchName} --follow-tags --verbose`
+        );
+        exec(`git branch -d ${tempPublishBranch}`);
+        exec(`git push origin --delete -d ${tempPublishBranch}`);
+      }
+    );
+  };
+
+  request.post(
+    {
+      url: "https://api.github.com/repos/Microsoft/react-native/releases",
+      headers: {
+        "User-Agent": userAgent,
+        Authorization: authHeader,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        tag_name: `v${releaseVersion}`,
+        target_commitish: publishBranchName,
+        name: `v${releaseVersion}`,
+        body: `v${releaseVersion}`,
+        draft: false,
+        prerelease: true
+      })
+    },
+    function(err, res, body) {
+      if (err) {
+        console.log(err);
+        throw new Error("Error fetching release id.");
+      }
+
+      console.log("Created GitHub Release: " + JSON.stringify(body, null, 2));
+      var releaseId = JSON.parse(body).id;
+      uploadReleaseAssetUrl = assetUpdateUrl.replace(/{id}/, releaseId);
+
+      uploadTarBallToRelease();
+    }
+  );
 }
 
 doPublish();
