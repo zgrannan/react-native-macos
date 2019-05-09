3c44dded8fbdc5e0c47b89ccb928309510c7934f
Anandraj Govindan <mganandraj@outlook.com>
2019-05-09 23:31:04 +0530

    Yoga refresh - copying from v0.59.8

--- ReactCommon/yoga/yoga/YGMarker.cpp
+++ ReactCommon/yoga/yoga/YGMarker.cpp
@@ -4,6 +4,11 @@
  * This source code is licensed under the MIT license found in the LICENSE
  * file in the root directory of this source tree.
  */
+#include "YGMarker.h"
 #include "YGConfig.h"
 
-YGConfig::YGConfig(YGLogger logger) : logger(logger) {}
+void YGConfigSetMarkerCallbacks(
+    YGConfigRef config,
+    YGMarkerCallbacks markerCallbacks) {
+  config->markerCallbacks = markerCallbacks;
+}
--- ReactCommon/yoga/yoga/YGValue.cpp
+++ ReactCommon/yoga/yoga/YGValue.cpp
@@ -4,6 +4,8 @@
  * This source code is licensed under the MIT license found in the LICENSE
  * file in the root directory of this source tree.
  */
-#include "YGConfig.h"
+#include "YGValue.h"
 
-YGConfig::YGConfig(YGLogger logger) : logger(logger) {}
+const YGValue YGValueZero = {0, YGUnitPoint};
+const YGValue YGValueUndefined = {YGUndefined, YGUnitUndefined};
+const YGValue YGValueAuto = {YGUndefined, YGUnitAuto};
