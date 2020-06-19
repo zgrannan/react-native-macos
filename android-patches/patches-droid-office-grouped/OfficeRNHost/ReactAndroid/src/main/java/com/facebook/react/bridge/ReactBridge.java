dabe72f76fc94ecd96a812d8618343c16755e130
Anandraj <anandrag@microsoft.com>
2020-06-19 10:06:06 -0700

    Fixing another OS-specific issue in publish script. And adding back a patch that we missed applying by mistake earlier. (#461)

--- android-patches/patches-droid-office-grouped/OfficeRNHost/ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java
+++ android-patches/patches-droid-office-grouped/OfficeRNHost/ReactAndroid/src/main/java/com/facebook/react/bridge/ReactBridge.java
@@ -1,6 +1,6 @@
 --- "e:\\github\\fb-react-native-forpatch-base\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\bridge\\ReactBridge.java"	2020-01-30 13:55:48.274578500 -0800
 +++ "e:\\github\\ms-react-native-forpatch\\ReactAndroid\\src\\main\\java\\com\\facebook\\react\\bridge\\ReactBridge.java"	2020-02-20 11:21:17.368516500 -0800
-@@ -28,6 +28,14 @@
+@@ -28,6 +28,15 @@
      sLoadStartTime = SystemClock.uptimeMillis();
      Systrace.beginSection(TRACE_TAG_REACT_JAVA_BRIDGE, "ReactBridge.staticInit::load:reactnativejni");
      ReactMarker.logMarker(ReactMarkerConstants.LOAD_REACT_NATIVE_SO_FILE_START);
@@ -12,6 +12,7 @@
 +    SoLoader.loadLibrary("glog_init");
 +    SoLoader.loadLibrary("fb");
 +    SoLoader.loadLibrary("yoga");
++    SoLoader.loadLibrary("jsinspector");
      SoLoader.loadLibrary("reactnativejni");
      ReactMarker.logMarker(ReactMarkerConstants.LOAD_REACT_NATIVE_SO_FILE_END);
      Systrace.endSection(TRACE_TAG_REACT_JAVA_BRIDGE);
